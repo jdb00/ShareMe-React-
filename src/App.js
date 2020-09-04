b661c98b2ecc5ebb0396ca63789c4367e2316fea
Jordan <jordan.broad1@gmail.com>
2020-09-04 15:38:02 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './sharemelogo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div className="app">
+        <header>
+            <i className="fas fa-chevron-left"></i>
+            <img src={logo} alt=""/>
+            <div className="user">
+                <img src="" alt=""/>
+            </div>
+        </header>
+        <main>
+            <div className="container">
+
+            </div>
+        </main>
+        <footer>
+            <i className="fas fa-home"></i>
+            <i className="fas fa-plus"></i>
+            <i className="fas fa-cog"></i>
+        </footer>
+      </div> 
+    );
+  }
 }
 
 export default App;
